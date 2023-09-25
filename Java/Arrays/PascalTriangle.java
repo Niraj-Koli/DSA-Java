374802c7c66220f49458d05b18d13f11c16b22a4
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-25 20:11:29 +0530

    Added PascalTriangle Files

---
+++ Java/Arrays/PascalTriangle.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            List<Integer> prevRow = triangle.get(i - 1);
+
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                int sum = prevRow.get(j - 1) + prevRow.get(j);
+
+                row.add(sum);
+            }
+
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+
+        List<List<Integer>> result = generate(numRows);
+
+        for (List<Integer> row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
