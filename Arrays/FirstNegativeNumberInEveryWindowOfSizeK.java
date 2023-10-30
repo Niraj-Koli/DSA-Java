0a23dc982db700e436cc93d80b87b5b7c4b7c68c
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-10-30 19:54:32 +0530

    Added FirstNegativeNumberInEveryWindowOfSizeK

---
+++ Arrays/FirstNegativeNumberInEveryWindowOfSizeK.java
@@ -0,0 +1,45 @@
+/* Find the first negative number in every window of size k return 0 if not */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstNegativeNumberInEveryWindowOfSizeK {
+    public static List<Integer> firstNegative(int[] nums, int k) {
+        int len = nums.length;
+
+        int i = 0;
+        int j = 0;
+
+        ArrayList<Integer> support = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<Integer>();
+
+        while (j < len) {
+            if (nums[j] < 0) {
+                support.add(nums[j]);
+            }
+
+            if (j - i + 1 == k) {
+                if (support.isEmpty()) {
+                    answer.add(0);
+                } else {
+                    answer.add(support.get(0));
+                }
+
+                support.remove(Integer.valueOf(nums[i]));
+                i++;
+            }
+            j++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 12, -1, -7, 8, -15, 30, 16, 28 };
+        int k = 3;
+
+        List<Integer> result = firstNegative(nums, k);
+
+        System.out.println(result);
+    }
+}
