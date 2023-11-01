3e823d39e0f864e4d6ea963b00ba8a785fffc222
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-01 19:16:30 +0530

    Added MaximumSumOfSubarrayOfSizeK

---
+++ Arrays/MaximumSumOfSubarrayOfSizeK.java
@@ -0,0 +1,36 @@
+/* Find the maximum sum of a subarray with size k */
+
+public class MaximumSumOfSubarrayOfSizeK {
+    public static int maxSum(int[] nums, int k) {
+        int len = nums.length;
+
+        int i = 0;
+        int j = 0;
+
+        int sum = 0;
+        int max = 0;
+
+        while (j < len) {
+            sum += nums[j];
+
+            if (j - i + 1 == k) {
+                max = Math.max(max, sum);
+
+                sum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 5, 1, 8, 2, 9, 1 };
+        int k = 3;
+
+        int result = maxSum(nums, k);
+
+        System.out.println(result);
+    }
+}
