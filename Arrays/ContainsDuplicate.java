db430c7f81fceed6fe016c7f9743f348449d9989
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-02 23:45:03 +0530

    Added ContainsDuplicate

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -25,9 +25,9 @@ public class ContainsDuplicate {
     public static void main(String[] args) {
         int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
 
-        boolean result = containsDuplicate(nums);
+        boolean answer = containsDuplicate(nums);
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
 
