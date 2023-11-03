1696974bef6bee46f1f71631ba5fb79766a059c9
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-03 20:35:01 +0530

    Added KadaneAlgorithm

--- Arrays/KadaneAlgorithm.java
+++ Arrays/KadaneAlgorithm.java
@@ -28,9 +28,9 @@ public class KadaneAlgorithm {
     public static void main(String[] args) {
         int[] nums = { 5, 4, -1, 7, 8 };
 
-        int result = maxSubArray(nums);
+        int answer = maxSubArray(nums);
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
 
