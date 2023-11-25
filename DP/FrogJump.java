3b4b63d9998bd4a4ff25439928d22c4e40533f63
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-25 18:55:09 +0530

    FrogJump

--- DP/FrogJump.java
+++ DP/FrogJump.java
@@ -8,19 +8,35 @@
  */
 
 public class FrogJump {
+    public static int[] t = new int[100000];
+
+    public static int solve(int i, int[] height) {
+        if (i == 0) {
+            return 0;
+        }
+
+        if (t[i] != 0)
+            return t[i];
+
+        int jumpOne = solve(i - 1, height) + Math.abs(height[i] - height[i - 1]);
+
+        int jumpTwo = i > 1 ? solve(i - 2, height) + Math.abs(height[i] - height[i - 2]) : Integer.MAX_VALUE;
+
+        t[i] = Math.min(jumpOne, jumpTwo);
+
+        return t[i];
+    }
+
     public static int frogJump(int[] heights) {
         int n = heights.length;
 
         int[] t = new int[n];
 
         for (int i = 1; i < n; i++) {
-            int jumpTwo = Integer.MAX_VALUE;
 
             int jumpOne = t[i - 1] + Math.abs(heights[i] - heights[i - 1]);
 
-            if (i > 1) {
-                jumpTwo = t[i - 2] + Math.abs(heights[i] - heights[i - 2]);
-            }
+            int jumpTwo = i > 1 ? t[i - 2] + Math.abs(heights[i] - heights[i - 2]) : Integer.MAX_VALUE;
 
             t[i] = Math.min(jumpOne, jumpTwo);
         }
