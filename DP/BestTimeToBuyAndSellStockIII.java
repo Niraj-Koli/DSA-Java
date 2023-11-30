10b995e0fcc34aaa6e3bb52768ace850947f5dd3
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-30 20:21:26 +0530

    BestTimeToBuyAndSellStockII

--- DP/BestTimeToBuyAndSellStockIII.java
+++ DP/BestTimeToBuyAndSellStockIII.java
@@ -14,23 +14,23 @@ public class BestTimeToBuyAndSellStockIII {
         int n = prices.length;
         int k = 2;
 
-        int[][][] t = new int[n + 1][2][k + 1];
+        int[][][] dp = new int[n + 1][2][k + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= 1; j++) {
                 for (int cap = 1; cap <= k; cap++) {
                     if (j == 0) {
-                        t[i][j][cap] = Math.max(0 + t[i + 1][0][cap],
-                                -prices[i] + t[i + 1][1][cap]);
+                        dp[i][j][cap] = Math.max(0 + dp[i + 1][0][cap],
+                                -prices[i] + dp[i + 1][1][cap]);
                     } else {
-                        t[i][j][cap] = Math.max(0 + t[i + 1][1][cap],
-                                prices[i] + t[i + 1][0][cap - 1]);
+                        dp[i][j][cap] = Math.max(0 + dp[i + 1][1][cap],
+                                prices[i] + dp[i + 1][0][cap - 1]);
                     }
                 }
             }
         }
 
-        return t[0][0][k];
+        return dp[0][0][k];
     }
 
     public static void main(String[] args) {
