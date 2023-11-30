1386b9dc15353b8944a037bc4fa367c82f3665d8
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-11-30 20:21:39 +0530

    BestTimeToBuyAndSellStockWithCooldown

--- DP/BestTimeToBuyAndSellStockWithCooldown.java
+++ DP/BestTimeToBuyAndSellStockWithCooldown.java
@@ -16,25 +16,25 @@ public class BestTimeToBuyAndSellStockWithCooldown {
     public static int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[][] t = new int[n + 2][2];
+        int[][] dp = new int[n + 2][2];
 
         int profit = 0;
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= 1; j++) {
                 if (j == 0) {
-                    profit = Math.max(0 + t[i + 1][0], -prices[i] + t[i + 1][1]);
+                    profit = Math.max(0 + dp[i + 1][0], -prices[i] + dp[i + 1][1]);
                 }
 
                 if (j == 1) {
-                    profit = Math.max(0 + t[i + 1][1], prices[i] + t[i + 2][0]);
+                    profit = Math.max(0 + dp[i + 1][1], prices[i] + dp[i + 2][0]);
                 }
 
-                t[i][j] = profit;
+                dp[i][j] = profit;
             }
         }
 
-        return t[0][0];
+        return dp[0][0];
     }
 
     public static void main(String[] args) {
