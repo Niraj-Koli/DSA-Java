b45f64b8868f39219c366b7d7cc7eba2c22d2c8f
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-02 10:45:26 +0530

    CoinChange

--- DP/CoinChange.java
+++ DP/CoinChange.java
@@ -13,35 +13,35 @@ public class CoinChange {
     public static int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
-        int[][] t = new int[n + 1][amount + 1];
+        int[][] dp = new int[n + 1][amount + 1];
 
         for (int j = 0; j < n + 1; j++) {
-            t[j][0] = 0;
+            dp[j][0] = 0;
         }
 
         for (int i = 0; i < amount + 1; i++) {
-            t[0][i] = Integer.MAX_VALUE - 1;
+            dp[0][i] = Integer.MAX_VALUE - 1;
         }
 
         for (int k = 1; k < amount + 1; k++) {
             if (k % coins[0] == 0) {
-                t[1][k] = k / coins[0];
+                dp[1][k] = k / coins[0];
             } else {
-                t[1][k] = Integer.MAX_VALUE - 1;
+                dp[1][k] = Integer.MAX_VALUE - 1;
             }
         }
 
         for (int i = 2; i < n + 1; i++) {
             for (int j = 1; j < amount + 1; j++) {
                 if (coins[i - 1] <= j) {
-                    t[i][j] = Math.min(t[i][j - coins[i - 1]] + 1, t[i - 1][j]);
+                    dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
                 } else {
-                    t[i][j] = t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return t[n][amount] == Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
     }
 
     public static void main(String[] args) {
