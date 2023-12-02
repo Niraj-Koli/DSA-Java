eb7541d24aada46218ffee7e392e810599b65691
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-02 10:45:31 +0530

    CoinChangeII

--- DP/CoinChangeII.java
+++ DP/CoinChangeII.java
@@ -14,27 +14,27 @@ public class CoinChangeII {
     public static int change(int amount, int[] coins) {
         int n = coins.length;
 
-        int[][] t = new int[n + 1][amount + 1];
+        int[][] dp = new int[n + 1][amount + 1];
 
         for (int i = 0; i < amount + 1; i++) {
-            t[0][i] = 0;
+            dp[0][i] = 0;
         }
 
         for (int j = 0; j < n + 1; j++) {
-            t[j][0] = 1;
+            dp[j][0] = 1;
         }
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < amount + 1; j++) {
                 if (coins[i - 1] <= j) {
-                    t[i][j] = t[i][j - coins[i - 1]] + t[i - 1][j];
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
                 } else {
-                    t[i][j] = t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return t[n][amount];
+        return dp[n][amount];
     }
 
     public static void main(String[] args) {
@@ -49,13 +49,13 @@ public class CoinChangeII {
 
 // class Solution {
 // public int change(int amount, int[] coins) {
-// int[] t = new int[amount + 1];
-// t[0] = 1;
+// int[] dp = new int[amount + 1];
+// dp[0] = 1;
 // for (int i = coins.length - 1; i >= 0; i--) {
 // for (int j = coins[i]; j <= amount; j++) {
-// t[j] += t[j - coins[i]];
+// dp[j] += dp[j - coins[i]];
 // }
 // }
-// return t[amount];
+// return dp[amount];
 // }
 // }
