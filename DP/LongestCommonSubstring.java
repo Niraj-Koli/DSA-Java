abb1ac3c9388f4886fbcc7b38990740153f43d7e
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-06 13:35:51 +0530

    LongestCommonSubstring

--- DP/LongestCommonSubstring.java
+++ DP/LongestCommonSubstring.java
@@ -7,16 +7,16 @@ public class LongestCommonSubstring {
     public static int longestCommonSubstr(String x, String y, int n, int m) {
         int max = 0;
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
 
-                    max = Math.max(max, t[i][j]);
+                    max = Math.max(max, dp[i][j]);
                 } else {
-                    t[i][j] = 0;
+                    dp[i][j] = 0;
                 }
             }
         }
