737f86c6a49ceb1a154554d596034fba7a7e8924
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-06 13:35:40 +0530

    LongestCommonSubsequence

--- DP/LongestCommonSubsequence.java
+++ DP/LongestCommonSubsequence.java
@@ -12,13 +12,13 @@
  */
 
 public class LongestCommonSubsequence {
-    public static int[][] t;
+    public static int[][] dp;
 
     public static int longestCommonSubsequenceRecursive(String x, String y) {
         int n = x.length();
         int m = y.length();
 
-        t = new int[n + 1][m + 1];
+        dp = new int[n + 1][m + 1];
 
         return lcs(x, y, n, m);
     }
@@ -28,18 +28,18 @@ public class LongestCommonSubsequence {
             return 0;
         }
 
-        if (t[n][m] != 0) {
-            return t[n][m];
+        if (dp[n][m] != 0) {
+            return dp[n][m];
         }
 
         if (x.charAt(n - 1) == y.charAt(m - 1)) {
-            t[n][m] = 1 + lcs(x, y, n - 1, m - 1);
+            dp[n][m] = 1 + lcs(x, y, n - 1, m - 1);
 
-            return t[n][m];
+            return dp[n][m];
         } else {
-            t[n][m] = Math.max(lcs(x, y, n, m - 1), lcs(x, y, n - 1, m));
+            dp[n][m] = Math.max(lcs(x, y, n, m - 1), lcs(x, y, n - 1, m));
 
-            return t[n][m];
+            return dp[n][m];
         }
     }
 
@@ -47,19 +47,19 @@ public class LongestCommonSubsequence {
         int n = x.length();
         int m = y.length();
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
 
-        return t[n][m];
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
@@ -77,21 +77,21 @@ public class LongestCommonSubsequence {
 // int n = text1.length();
 // int m = text2.length();
 
-// int[] t = new int[m + 1];
+// int[] dp = new int[m + 1];
 
 // for (int i = 1; i <= n; i++) {
 // int prev = 0;
 // for (int j = 1; j <= m; j++) {
-// int temp = t[j];
+// int temp = dp[j];
 // if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-// t[j] = prev + 1;
+// dp[j] = prev + 1;
 // } else {
-// t[j] = Math.max(t[j], t[j - 1]);
+// dp[j] = Math.max(dp[j], dp[j - 1]);
 // }
 // prev = temp;
 // }
 // }
 
-// return t[m];
+// return dp[m];
 // }
 // }
