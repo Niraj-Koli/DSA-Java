0c556a9f245ded64d59e8e5dac566623b3824837
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-06 13:36:03 +0530

    LongestPalindromicSubsequence

--- DP/LongestPalindromicSubsequence.java
+++ DP/LongestPalindromicSubsequence.java
@@ -11,19 +11,19 @@ public class LongestPalindromicSubsequence {
         int n = x.length();
         int m = y.length();
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
 
-        return t[n][m];
+        return dp[n][m];
     }
 
     public static int longestPalindromeSubseq(String s) {
