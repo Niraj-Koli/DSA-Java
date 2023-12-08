8b66c028995c2a8ee2c7250b8c6bc432f1047554
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-08 19:12:04 +0530

    MinimumInsertionStepsToMakeAStringPalindrome

--- DP/MinimumInsertionStepsToMakeAStringPalindrome.java
+++ DP/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -12,19 +12,19 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
         int n = x.length();
         int m = y.length();
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
 
-        return t[n][m];
+        return dp[n][m];
     }
 
     public static int lps(String s) {
@@ -58,17 +58,17 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
 // public int longestPalindromeSubseq(String s) {
 // char[] c = s.toCharArray();
 
-// int[] t = new int[c.length];
+// int[] dp = new int[c.length];
 
-// for (int j = 0; j < t.length; j++) {
-// t[j] = 1;
+// for (int j = 0; j < dp.length; j++) {
+// dp[j] = 1;
 // int topRight = 0;
 // for (int i = j - 1; i >= 0; i--) {
-// int temp = t[i];
-// t[i] = c[i] == c[j] ? 2 + topRight : Math.max(t[i], t[i + 1]);
+// int temp = dp[i];
+// dp[i] = c[i] == c[j] ? 2 + topRight : Math.max(dp[i], dp[i + 1]);
 // topRight = temp;
 // }
 // }
-// return t[0];
+// return dp[0];
 // }
 // }
