a2d9edaa1c6b2400f1dcae16ee6fc75f57fbbaf3
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-09 12:41:08 +0530

    MinimumSubsetSumPartitionDifference

--- DP/MinimumSubsetSumPartitionDifference.java
+++ DP/MinimumSubsetSumPartitionDifference.java
@@ -14,22 +14,22 @@ public class MinimumSubsetSumPartitionDifference {
             sum += num;
         }
 
-        boolean[][] t = new boolean[n + 1][sum + 1];
+        boolean[][] dp = new boolean[n + 1][sum + 1];
 
         for (int i = 0; i < sum + 1; i++) {
-            t[0][i] = false;
+            dp[0][i] = false;
         }
 
         for (int j = 0; j < n + 1; j++) {
-            t[j][0] = true;
+            dp[j][0] = true;
         }
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
                 if (nums[i - 1] <= j) {
-                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
                 } else {
-                    t[i][j] = t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
@@ -37,7 +37,7 @@ public class MinimumSubsetSumPartitionDifference {
         int min = Integer.MAX_VALUE;
 
         for (int j = sum / 2; j >= 0; j--) {
-            if (t[n][j] == true) {
+            if (dp[n][j] == true) {
                 min = sum - 2 * j;
                 break;
             }
