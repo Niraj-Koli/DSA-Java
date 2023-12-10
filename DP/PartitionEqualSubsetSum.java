89c5bea5f8a2bb61f71721226b33902eb18c16f6
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-10 16:03:03 +0530

    PartitionEqualSubsetSum

--- DP/PartitionEqualSubsetSum.java
+++ DP/PartitionEqualSubsetSum.java
@@ -8,27 +8,27 @@ public class PartitionEqualSubsetSum {
     public static boolean isSubsetSum(int[] nums, int sum) {
         int n = nums.length;
 
-        boolean[][] t = new boolean[n + 1][sum + 1];
+        boolean[][] dp = new boolean[n + 1][sum + 1];
 
         for (int i = 0; i < sum + 1; i++) {
-            t[0][i] = false;
+            dp[0][i] = false;
         }
 
         for (int j = 0; j < n + 1; j++) {
-            t[j][0] = true;
+            dp[j][0] = true;
         }
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
                 if (nums[i - 1] <= j) {
-                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
                 } else {
-                    t[i][j] = t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return t[n][sum];
+        return dp[n][sum];
     }
 
     public static boolean canPartition(int[] nums) {
@@ -70,15 +70,15 @@ public class PartitionEqualSubsetSum {
 
 // int targetSum = totalSum / 2;
 
-// boolean[] t = new boolean[targetSum + 1];
-// t[0] = true;
+// boolean[] dp = new boolean[targetSum + 1];
+// dp[0] = true;
 
 // for (int num : nums) {
 // for (int j = targetSum; j >= num; j--) {
-// t[j] = t[j] || t[j - num];
+// dp[j] = dp[j] || dp[j - num];
 // }
 // }
 
-// return t[targetSum];
+// return dp[targetSum];
 // }
 // }
