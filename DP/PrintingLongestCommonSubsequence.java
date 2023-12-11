fa1352ceabb7ce7e30cd570154c679fc4befd055
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-11 15:44:25 +0530

    PrintingLongestCommonSubsequence

--- DP/PrintingLongestCommonSubsequence.java
+++ DP/PrintingLongestCommonSubsequence.java
@@ -7,14 +7,14 @@ public class PrintingLongestCommonSubsequence {
         int n = x.length();
         int m = y.length();
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
@@ -31,7 +31,7 @@ public class PrintingLongestCommonSubsequence {
                 i--;
                 j--;
             } else {
-                if (t[i][j - 1] > t[i - 1][j]) {
+                if (dp[i][j - 1] > dp[i - 1][j]) {
                     j--;
                 } else {
                     i--;
