b269d1fdc7ea593bdd390b2113a2733af2cfd7c5
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-11 15:44:35 +0530

    RodCutting

--- DP/RodCutting.java
+++ DP/RodCutting.java
@@ -16,20 +16,20 @@ public class RodCutting {
 
         int m = length.length;
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (length[i - 1] <= j) {
-                    t[i][j] = Math.max(price[i - 1] + t[i][j - length[i - 1]], t[i - 1][j]);
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
                 } else {
-                    t[i][j] = t[i - 1][j];
+                    dp[i][j] = dp[i - 1][j];
 
                 }
             }
         }
 
-        return t[n][m];
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
