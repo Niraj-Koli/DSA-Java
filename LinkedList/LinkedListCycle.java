0963078670627f23604ebb34300ca0c6a4c36501
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-22 19:54:06 +0530

    LinkedListCycle

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,73 @@
+/*
+ * Given head, the head of a linked list, determine if the linked list has a
+ * cycle in it.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is connected
+ * to. Note that pos is not passed as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // int[] head = { 3, 2, 0, -4 };
+
+        // boolean answer = hasCycle(head);
+
+        // System.out.println(answer);
+    }
+}
+
+// public class Solution {
+// public boolean hasCycle(ListNode head) {
+// Set<ListNode> seen = new HashSet<>();
+
+// while (head != null) {
+// if (seen.contains(head))
+// return true;
+
+// seen.add(head);
+
+// head = head.next;
+// }
+
+// return false;
+
+// }
+// }
