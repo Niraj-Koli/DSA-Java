9a3dc837b848eac6c1e5beb8391cf07f19088fef
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-22 19:54:16 +0530

    LinkedListCycleII

---
+++ LinkedList/LinkedListCycleII.java
@@ -0,0 +1,84 @@
+/*
+ * Given the head of a linked list, return the node where the cycle begins. If
+ * there is no cycle, return null.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is connected
+ * to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as
+ * a parameter.
+ * 
+ * Do not modify the linked list.
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LinkedListCycleII {
+    public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// public class Solution {
+// public ListNode detectCycle(ListNode head) {
+// ListNode slow = head;
+// ListNode fast = head;
+
+// while (fast != null && fast.next != null) {
+// slow = slow.next;
+// fast = fast.next.next;
+
+// if (slow == fast) {
+
+// slow = head;
+// while (slow != fast) {
+// slow = slow.next;
+// fast = fast.next;
+// }
+// return slow;
+// }
+// }
+
+// System.gc();
+// return null;
+// }
+// }
