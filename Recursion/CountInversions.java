2019a6119f9a831200465e84867976eab6fa6a81
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-12-29 12:47:50 +0530

    CountInversions

---
+++ Recursion/CountInversions.java
@@ -0,0 +1,81 @@
+/*
+ * Given an array of integers. Find the Inversion Count in the array.
+ * 
+ * Inversion Count: For an array, inversion count indicates how far (or close)
+ * the array is from being sorted. If the array is already sorted then the
+ * inversion count is 0.
+ * If an array is sorted in the reverse order then the inversion count is the
+ * maximum.
+ * Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i <
+ * j.
+ */
+
+import java.util.ArrayList;
+
+public class CountInversions {
+    public static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+
+        int left = low;
+        int right = mid + 1;
+
+        int count = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                count += (mid - left + 1);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+
+        return count;
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int count = 0;
+
+        if (low >= high) {
+            return count;
+        }
+
+        int mid = (low + high) / 2;
+
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+
+        count += merge(arr, low, mid, high);
+
+        return count;
+    }
+
+    public static int numberOfInversions(int[] arr, int n) {
+        return mergeSort(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 1, 3, 5 };
+        int n = 5;
+
+        int answer = numberOfInversions(arr, n);
+
+        System.out.println(answer);
+    }
+}
