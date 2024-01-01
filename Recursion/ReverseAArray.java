4322c7b39cdcce0ddfd3e2cda9d91ad388cd3620
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-01 22:58:20 +0530

    ReverseAArray

---
+++ Recursion/ReverseAArray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ReverseAArray {
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void reverse(int[] nums, int i) {
+        int n = nums.length;
+
+        if (i >= n / 2) {
+            return;
+        }
+
+        swap(nums, i, n - 1 - i);
+
+        reverse(nums, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5 };
+
+        System.out.println(Arrays.toString(nums));
+
+        reverse(nums, 0);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
