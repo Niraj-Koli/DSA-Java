d8474dab8eb873eb792f1d5401903817743fd82e
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-08 11:25:36 +0530

    CheckIfABitIsSet

---
+++ Bits/CheckIfABitIsSet.java
@@ -0,0 +1,15 @@
+public class CheckIfABitIsSet {
+    public static void main(String[] args) {
+        int n = 12;
+        int i = 3;
+
+        boolean set = ((1 << i) & n) != 0;
+        // boolean set = ((n >> i) & 1) != 0;
+
+        if (set) {
+            System.out.println("ith Bit Is Set");
+        } else {
+            System.out.println("ith Bit Is Not Set");
+        }
+    }
+}
