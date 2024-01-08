4b67dbb22528d155f1e4079812a2bbac7ce5ccb7
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-08 11:25:42 +0530

    CheckIfANumberIsPowerOf2

---
+++ Bits/CheckIfANumberIsPowerOf2.java
@@ -0,0 +1,15 @@
+public class CheckIfANumberIsPowerOf2 {
+    public static void main(String[] args) {
+        int n = 16;
+
+        boolean isPower = (n & n - 1) == 0;
+
+        if (isPower) {
+            System.err.println(n + " is power of 2");
+        } else {
+            System.out.println(n + " is not the power of 2");
+        }
+
+        // Delete last set bit int bit = (n & n - 1); //
+    }
+}
