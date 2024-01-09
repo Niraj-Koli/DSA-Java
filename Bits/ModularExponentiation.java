e411cc84caee26a276e79ff10731881b54b5fde8
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-09 10:16:12 +0530

    ModularExponentiation

---
+++ Bits/ModularExponentiation.java
@@ -0,0 +1,34 @@
+public class ModularExponentiation {
+    
+    // Time -> O(log y) //
+    // Space -> O(1) //
+
+    public static int power(int x, int y, int p) {
+        int res = 1;
+
+        x = x % p;
+
+        if (x == 0) {
+            return 0;
+        }
+
+        while (y > 0) {
+            if ((y & 1) != 0) {
+                res = (res * x) % p;
+            }
+
+            y = y >> 1;
+            x = (x * x) % p;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int x = 2312, y = 3434, p = 6789;
+
+        int mod = power(x, y, p);
+
+        System.out.println(mod);
+    }
+}
