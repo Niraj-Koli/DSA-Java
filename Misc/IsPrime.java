c967f83961421408a3a7d84f3a63bd31425a777d
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-12 11:56:58 +0530

    IsPrime

---
+++ Misc/IsPrime.java
@@ -0,0 +1,31 @@
+public class IsPrime {
+
+    // Time -> O(sqrt(n)) //
+    // Space -> O(1) //
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 13;
+
+        boolean answer = isPrime(number);
+
+        System.out.println(answer);
+    }
+}
