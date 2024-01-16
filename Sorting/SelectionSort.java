2f9e6196599f95cec582269460f9165fe58e36a7
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-16 12:32:51 +0530

    SelectionSort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+public class SelectionSort {
+
+    // Time -> O(n^2) //
+    // Space -> O(1) //
+
+    public static void selectionSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 10, 7, 21, 47, 73, 91, 36, 52, 2, 69 };
+
+        selectionSort(nums);
+
+        for (int number : nums) {
+            System.out.print(number + " ");
+        }
+    }
+}
