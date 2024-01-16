a78ef7a1584fff1a22af4d78b691d3deea3e489d
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-16 12:32:46 +0530

    RadixSort

---
+++ Sorting/RadixSort.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class RadixSort {
+
+    // Time -> O(nk) //
+    // Space -> O(n + k) //
+
+    public static void radixSort(int[] arr) {
+        int max = Arrays.stream(arr).max().orElse(0);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    public static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 10, 7, 21, 47, 73, 91, 36, 52, 2, 69 };
+
+        radixSort(nums);
+
+        for (int number : nums) {
+            System.out.print(number + " ");
+        }
+    }
+}
