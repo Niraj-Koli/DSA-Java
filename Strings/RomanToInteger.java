3da29ed293af2b72c46b6ac9db5fca3b96f56da3
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-01-22 12:49:57 +0530

    RomanToInteger

---
+++ Strings/RomanToInteger.java
@@ -0,0 +1,94 @@
+/*
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+ * and M.
+ * 
+ * Symbol Value
+ * I 1
+ * V 5
+ * X 10
+ * L 50
+ * C 100
+ * D 500
+ * M 1000
+ * For example, 2 is written as II in Roman numeral, just two ones added
+ * together. 12 is written as XII, which is simply X + II. The number 27 is
+ * written as XXVII, which is XX + V + II.
+ * 
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making four.
+ * The same principle applies to the number nine, which is written as IX. There
+ * are six instances where subtraction is used:
+ * 
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ */
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static int romanToInt(String str) {
+        int n = str.length();
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && map.get(str.charAt(i)) < map.get(str.charAt(i + 1))) {
+                result -= map.get(str.charAt(i));
+            } else {
+                result += map.get(str.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+
+        int answer = romanToInt(s);
+
+        System.out.println(answer);
+    }
+}
+
+class Solution {
+    public int romanToInt(String s) {
+
+        int res = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        s = s.replace("IV", "IIII");
+        s = s.replace("IX", "VIIII");
+        s = s.replace("XL", "XXXX");
+        s = s.replace("XC", "LXXXX");
+        s = s.replace("CD", "CCCC");
+        s = s.replace("CM", "DCCCC");
+
+        for (int i = 0; i < s.length(); i++) {
+
+            res = res + (map.get(s.charAt(i)));
+        }
+
+        return res;
+
+    }
+}
