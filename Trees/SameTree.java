c8cbe7a1c86a37c1a1da5f4423cac1ed314eb73b
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-02-06 23:35:36 +0530

    SameTree

---
+++ Trees/SameTree.java
@@ -0,0 +1,50 @@
+/*
+ * Given the roots of two binary trees p and q, write a function to check if
+ * they are the same or not.
+ * 
+ * Two binary trees are considered the same if they are structurally identical,
+ * and the nodes have the same value.
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return (p == q);
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        boolean answer = isSameTree(p, q);
+
+        System.out.println(answer);
+    }
+}
