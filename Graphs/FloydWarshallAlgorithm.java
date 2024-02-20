09b0603405603541bcdeb2d41778da7917b588ab
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-02-20 08:58:39 +0530

    FloydWarshallAlgorithm

---
+++ Graphs/FloydWarshallAlgorithm.java
@@ -0,0 +1,54 @@
+// Time -> O(N^3)
+// Space -> O(N^2)
+
+public class FloydWarshallAlgorithm {
+    public static void floydWarshall(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = (int) (1e9);
+                }
+
+                if (i == j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == (int) (1e9)) {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 0, 2, -1, -1 },
+                { 1, 0, 3, -1 },
+                { -1, -1, -1, -1 },
+                { 3, 5, 4, 0 },
+        };
+
+        floydWarshall(matrix);
+
+        for (int[] row : matrix) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
