14b807687c3952abe70fb66cd148adc52fe62e0d
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-02-23 16:20:21 +0530

    NumberOfDistinctIslands

---
+++ Graphs/NumberOfDistinctIslands.java
@@ -0,0 +1,75 @@
+/*
+ * Given a boolean 2D matrix grid of size n * m. You have to find the number of
+ * distinct islands where a group of connected 1s (horizontally or vertically)
+ * forms an island. Two islands are considered to be distinct if and only if one
+ * island is not equal to another (not rotated or reflected).
+ */
+
+// Time -> O(N x M)
+// Space -> O(N x M)
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class NumberOfDistinctIslands {
+    public static String toSting(int row, int col) {
+        return Integer.toString(row) + " " + Integer.toString(col);
+    }
+
+    public static void dfs(int row, int col, int row0, int col0, boolean[][] vis, int[][] grid, List<String> island,
+            int[] dx, int[] dy) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        vis[row][col] = true;
+        island.add(toSting(row - row0, col - col0));
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+
+            boolean bounds = (nrow >= 0 && nrow < n) && (ncol >= 0 && ncol < m);
+
+            if (bounds && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+                dfs(nrow, ncol, row0, col0, vis, grid, island, dx, dy);
+            }
+        }
+    }
+
+    public static int countDistinctIslands(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+
+        HashSet<ArrayList<String>> set = new HashSet<ArrayList<String>>();
+
+        int[] dx = { -1, 0, 1, 0 };
+        int[] dy = { 0, -1, 0, 1 };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == 1) {
+                    ArrayList<String> island = new ArrayList<String>();
+                    dfs(i, j, i, j, vis, grid, island, dx, dy);
+                    set.add(island);
+                }
+            }
+        }
+        return set.size();
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                { 1, 1, 0, 1, 1 },
+                { 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1 },
+                { 1, 1, 0, 1, 1 },
+        };
+
+        int ans = countDistinctIslands(grid);
+
+        System.out.println(ans);
+    }
+}
