02354f581c3f85c918eac7cb2bea88b1dd00bbd4
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-02-28 09:45:15 +0530

    FourSum

---
+++ Arrays/FourSum.java
@@ -0,0 +1,76 @@
+/*
+ * Given an array nums of n integers, return an array of all the unique
+ * quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ * 
+ * 0 <= a, b, c, d < n
+ * a, b, c, and d are distinct.
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * You may return the answer in any order.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FourSum {
+
+    // Time -> O(n^3)
+    // Space -> O(n^2)
+
+    private static ArrayList<ArrayList<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else if (sum == target) {
+                        res.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], nums[l])));
+
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, -1, 0, -2, 2 };
+        int target = 0;
+
+        System.out.println(fourSum(nums, target));
+    }
+}
