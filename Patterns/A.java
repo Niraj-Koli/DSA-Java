a6345ffa4661c1f42437490cb0cb96936cce53e8
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-04 10:57:21 +0530

    A

---
+++ Patterns/A.java
@@ -0,0 +1,16 @@
+public class A {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
