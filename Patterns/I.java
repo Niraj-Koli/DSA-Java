6602b315474683268c24b1811960333b69975c4e
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-07 12:25:35 +0530

    I

---
+++ Patterns/I.java
@@ -0,0 +1,44 @@
+public class I {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void invertedPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        pattern(n);
+        invertedPattern(n);
+    }
+}
