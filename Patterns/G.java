29f04beac3b7e01150cdffd1c7d11fe944b42b41
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-07 12:25:24 +0530

    G

---
+++ Patterns/G.java
@@ -0,0 +1,25 @@
+public class G {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        pattern(n);
+    }
+}
