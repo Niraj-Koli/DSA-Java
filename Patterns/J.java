7fbb81da1e7b086d4755a11496d01e6664879242
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-07 12:25:42 +0530

    J

---
+++ Patterns/J.java
@@ -0,0 +1,23 @@
+public class J {
+    public static void pattern(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+
+            if (i > n) {
+                stars = 2 * n - i;
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
