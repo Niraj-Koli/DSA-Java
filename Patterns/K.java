3e57adfbe8e572cb7032b902b571845bc4624ff3
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-08 11:40:09 +0530

    K

---
+++ Patterns/K.java
@@ -0,0 +1,18 @@
+public class K {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            int xor = i % 2 == 0 ? 1 : 0;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(xor);
+                xor ^= 1;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
