e09f10800b27db37c213889447e58628d432ef90
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-08 11:40:24 +0530

    M

---
+++ Patterns/M.java
@@ -0,0 +1,19 @@
+public class M {
+    public static void pattern(int n) {
+        int number = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
