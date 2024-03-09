219131c3dd310879887244c16680154c31428268
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-09 13:01:28 +0530

    R

---
+++ Patterns/R.java
@@ -0,0 +1,16 @@
+public class R {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (char ch = (char) (int) ('A' + n - 1 - i); ch <= (char) (int) ('A' + n - 1); ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
