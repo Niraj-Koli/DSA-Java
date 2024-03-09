db8089c6c755f9df6fdce54d2690d3e26ea58b51
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-09 13:01:23 +0530

    Q

---
+++ Patterns/Q.java
@@ -0,0 +1,34 @@
+public class Q {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print(ch);
+
+                if (j <= breakpoint) {
+                    ch++;
+                } else {
+                    ch--;
+                }
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
