8bb0d5dd9e9bbbce2c89b915ab527671ecb75b95
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-09 13:01:17 +0530

    P

---
+++ Patterns/P.java
@@ -0,0 +1,16 @@
+public class P {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (int) ('A' + i));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
