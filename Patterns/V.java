e067e3ccf049e682998b4a0fcfae0fe699c62b8c
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-10 16:21:35 +0530

    V

---
+++ Patterns/V.java
@@ -0,0 +1,21 @@
+public class V {
+    public static void pattern(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                int top = i;
+                int bottom = j;
+                int right = (2 * n - 2) - j;
+                int left = (2 * n - 2) - i;
+
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
