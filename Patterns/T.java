ddad8d6b7981ca4a60e9121a4d99c7ddf3f63742
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-10 16:21:23 +0530

    T

---
+++ Patterns/T.java
@@ -0,0 +1,38 @@
+public class T {
+    public static void pattern(int n) {
+        int spaces = 2 * n - 2;
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+
+            if (i > n)
+                stars = 2 * n - i;
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            if (i < n) {
+                spaces -= 2;
+            } else {
+                spaces += 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        pattern(n);
+    }
+}
