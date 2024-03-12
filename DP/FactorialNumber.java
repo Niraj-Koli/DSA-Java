fe26150cb353349d3f4c60d157e749ec64951576
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-12 12:42:53 +0530

    FactorialNumber

---
+++ DP/FactorialNumber.java
@@ -0,0 +1,18 @@
+public class FactorialNumber {
+    private static int factorial(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] * i;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(factorial(n));
+    }
+}
