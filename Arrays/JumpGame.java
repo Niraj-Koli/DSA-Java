ec1f96dd99aae281ef89ec75de9ca37ea3246a9e
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-12 12:43:27 +0530

    JumpGame

---
+++ Arrays/JumpGame.java
@@ -0,0 +1,30 @@
+/*
+ * You are given an integer array nums. You are initially positioned at the
+ * array's first index, and each element in the array represents your maximum
+ * jump length at that position.
+ * 
+ * Return true if you can reach the last index, or false otherwise.
+ */
+
+public class JumpGame {
+
+    // Time -> O(n)
+    // Space -> O(1)
+
+    private static boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        int i = 0;
+        for (int reach = 0; i < n && i <= reach; i++) {
+            reach = Math.max(reach, i + nums[i]);
+        }
+
+        return i == n;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 1, 1, 4 };
+
+        System.out.println(canJump(nums));
+    }
+}
