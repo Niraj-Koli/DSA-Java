37108f9ea3d70f892576787a80fb06a5e6318f47
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-12 12:43:22 +0530

    FactorialNumbersNotGreaterThanN

---
+++ Arrays/FactorialNumbersNotGreaterThanN.java
@@ -0,0 +1,39 @@
+/*
+ * You are given an integer ’n’.
+ * 
+ * Your task is to return a sorted array (in increasing order) containing all
+ * the factorial numbers which are less than or equal to ‘n’.
+ * 
+ * The factorial number is a factorial of a positive integer, like 24 is a
+ * factorial number, as it is a factorial of 4.
+ */
+
+import java.util.ArrayList;
+
+public class FactorialNumbersNotGreaterThanN {
+
+    // Time -> O(n) //
+    // Space -> O(n) //
+
+    private static ArrayList<Long> factorialNumbers(long n) {
+        ArrayList<Long> res = new ArrayList<Long>();
+
+        long fact = 1;
+        int count = 1;
+
+        while (fact <= n / count) {
+            fact *= count;
+
+            res.add(fact);
+            count++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+
+        System.out.println(factorialNumbers(n));
+    }
+}
