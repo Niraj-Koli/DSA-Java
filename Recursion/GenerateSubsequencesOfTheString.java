4817137a42cc64f155832b019c8124c80a8da155
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-16 21:56:05 +0530

    GenerateSubsequencesOfTheString

---
+++ Recursion/GenerateSubsequencesOfTheString.java
@@ -0,0 +1,34 @@
+/* Given a string, find all the possible subsequences of the string. */
+
+import java.util.ArrayList;
+
+public class GenerateSubsequencesOfTheString {
+
+    // Time -> O(2^n) //
+    // Space -> O(n) //
+
+    private static void solve(String s, int index, String temp, ArrayList<String> res) {
+        if (index == s.length()) {
+            res.add(temp);
+            return;
+        }
+
+        solve(s, index + 1, temp + s.charAt(index), res);
+
+        solve(s, index + 1, temp, res);
+    }
+
+    private static ArrayList<String> generateSubsequences(String s) {
+        ArrayList<String> res = new ArrayList<String>();
+
+        solve(s, 0, "", res);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+
+        System.out.println(generateSubsequences(s));
+    }
+}
