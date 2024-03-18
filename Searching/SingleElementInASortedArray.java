6a8cb4595151227f761b0182a0e8351100e3ecbb
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-18 11:34:04 +0530

    SingleElementInASortedArray

---
+++ Searching/SingleElementInASortedArray.java
@@ -0,0 +1,40 @@
+/*
+ * You are given a sorted array consisting of only integers where every element
+ * appears exactly twice, except for one element which appears exactly once.
+ * 
+ * Return the single element that appears only once.
+ * 
+ * Your solution must run in O(log n) time and O(1) space.
+ */
+
+public class SingleElementInASortedArray {
+
+    // Time -> O(log(n))
+    // Space -> O(1)
+
+    private static int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1])
+                    || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 3, 7, 7, 10, 11, 11 };
+
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
