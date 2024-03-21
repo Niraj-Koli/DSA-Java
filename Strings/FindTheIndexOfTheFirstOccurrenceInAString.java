d4d46f36c58e976fca7172ec68ff3b0eb60fae14
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-21 11:44:57 +0530

    FindTheIndexOfTheFirstOccurrenceInAString

---
+++ Strings/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,21 @@
+/*
+ * Given two strings needle and haystack, return the index of the first
+ * occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    // Time -> O(n * m) //
+    // Space -> O(1) //
+
+    private static int strStr(String haystack, String needle) {
+        return haystack.contains(needle) ? haystack.indexOf(needle) : -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        System.out.println(strStr(haystack, needle));
+    }
+}
