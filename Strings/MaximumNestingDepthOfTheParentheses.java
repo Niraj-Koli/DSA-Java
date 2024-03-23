5d2c6daeb71f8944919f5c83568be763f97e670c
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-23 13:34:58 +0530

    MaximumNestingDepthOfTheParentheses

---
+++ Strings/MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,52 @@
+/*
+ * A string is a valid parentheses string (denoted VPS) if it meets one of the
+ * following:
+ * 
+ * It is an empty string "", or a single character not equal to "(" or ")",
+ * It can be written as AB (A concatenated with B), where A and B are VPS's, or
+ * It can be written as (A), where A is a VPS.
+ * We can similarly define the nesting depth depth(S) of any VPS S as follows:
+ * 
+ * depth("") = 0
+ * depth(C) = 0, where C is a string with a single character not equal to "(" or
+ * ")".
+ * depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+ * depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+ * For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1,
+ * and 2), and ")(" and "(()" are not VPS's.
+ * 
+ * Given a VPS represented as string s, return the nesting depth of s.
+ */
+
+public class MaximumNestingDepthOfTheParentheses {
+
+    // Time -> O(n) //
+    // Space -> O(1) //
+
+    private static int maxDepth(String s) {
+        int n = s.length();
+
+        int depth = 0;
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                depth++;
+            } else if (ch == ')') {
+                depth--;
+            }
+
+            res = Math.max(res, depth);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+
+        System.out.println(maxDepth(s));
+    }
+}
