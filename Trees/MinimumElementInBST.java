498f0b2bc76e1d365070fc5e22e6c1fa9bad9760
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-03-25 19:39:06 +0530

    MinimumElementInBST

---
+++ Trees/MinimumElementInBST.java
@@ -0,0 +1,38 @@
+public class MinimumElementInBST {
+    static class TreeNode {
+        private int data;
+        private TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Time -> O(h) //
+    // Space -> O(1) //
+
+    private static int minValue(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root.data;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(minValue(root));
+    }
+}
